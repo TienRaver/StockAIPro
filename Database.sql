1a70fea857852f3c970cc39567f61ec26d99bab1
Tien Raver <tienraver@gmail.com>
2025-06-29 21:19:56 +0700

    first commit

---
+++ Database.sql
@@ -0,0 +1,27 @@
+﻿-- Phan 1: Tao co so du lieu database
+create database StockAIPro
+on
+(name ='StockAIPro_data',
+filename = 'F:\StockAIPro\StockAIPro_data.mdf',
+size = 10MB,
+maxsize = 500MB,
+filegrowth = 5MB
+)
+log on
+(name ='StockAIPro_log',
+filename = 'F:\StockAIPro\StockAIPro_log.ldf',
+size = 10MB,
+maxsize = 500MB,
+filegrowth = 5MB
+)
+	SELECT *
+	FROM kehoach
+
+	create table Employee(
+	Groupname varchar(20) null,
+	ID varchar(20) not null primary key,
+	Fullname varchar(255) not null,
+	Position varchar (255) null,
+	Dept varchar(255) null,
+	Shift_day varchar(20) null
+	)
